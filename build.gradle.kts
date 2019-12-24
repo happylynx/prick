206f009705debfc07a452f9ae3ed2db98587f445
happylynx <happylynx>
2019-12-24 15:07:54 +0100

    gradle: native image build
    * tested on Windows
    * working with GraalVM 19.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
+import java.nio.file.Path
+import java.nio.file.Files
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val kotlinVersion = "1.3.61"
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.3.21"
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
     id("application")
 }
 
@@ -19,10 +23,12 @@ repositories {
 }
 
 dependencies {
+    val picocliVersion = "4.1.4"
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
-    implementation(group = "info.picocli", name = "picocli", version = "3.9.5")
-//    testCompile group: 'junit', name: 'junit', version: '4.12'
+    implementation(group = "info.picocli", name = "picocli", version = picocliVersion)
+    // for Java it would be: annotationProcessor(group = "info.picocli", name = "picocli-codegen", version = picocliVersion)
+    kapt(group = "info.picocli", name = "picocli-codegen", version = picocliVersion)
 
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.4.1")
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = "5.4.1")
@@ -80,8 +86,63 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
+
+    register<Exec>("nativeImage") {
+        dependsOn(jar)
+        commandLine(getNativeImageCommandLine())
+        println("cmdline: ${commandLine.joinToString(" ")}")
+        doFirst {
+            Files.createDirectories(getNativeImageDir())
+        }
+    }
 }
 
 application {
     mainClassName = "com.github.happylynx.prick.cli.Main"
+}
+
+fun getNativeImageDir() = buildDir.resolve("native-image").toPath()
+
+fun getNativeImageCommandLine(): List<String> {
+    if (!System.getenv().containsKey("GRAALVM_HOME")) {
+        throw GradleException("Please set 'GRAALVM_HOME' environment variable.")
+    }
+
+    val nativeImagePath = Path.of(System.getenv("GRAALVM_HOME"))
+            .resolve("bin")
+            .resolve("native-image")
+            .toString()
+    val jarPath = tasks.jar.get().archiveFile.get().asFile.absolutePath
+    val execPath = getNativeImageDir().resolve("prick").toString()
+    val result = listOf(nativeImagePath, "-jar", jarPath, execPath)
+    if (isWindows()) {
+       return wrapForWindows(result)
+    }
+    return result
+}
+
+fun getRuntimeClasspath(): String {
+    val pathSeparator = System.getProperty("path.separator")
+    val jars = ArrayList<File>()
+    jars.add(tasks.jar.get().archiveFile.get().asFile)
+    jars.addAll(configurations.getByName("runtimeClasspath").files)
+    return jars.joinToString(pathSeparator) { it.canonicalPath }
+}
+
+fun wrapForWindows(commandLineToWrap: List<String>): List<String> {
+    val vsDirEnvVar = "VS_DIR"
+    val vsDir = if (System.getenv().containsKey(vsDirEnvVar))
+        System.getenv(vsDirEnvVar)
+    else
+        "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools"
+    val vcvarsPath = Path.of(vsDir).resolve("VC\\Auxiliary\\Build\\vcvars64.bat")
+    if (!Files.exists(vcvarsPath)) {
+        throw GradleException("Please make sure that Visual Studio 2017 Visual C++ build tools are correctly installed and environment variable VS_DIR is set.")
+    }
+    val originalNativeImageCall = commandLineToWrap.joinToString(" ")
+    return listOf("cmd", "/C", "call \"$vcvarsPath\" & $originalNativeImageCall")
+}
+
+fun isWindows(): Boolean {
+    return System.getProperty("os.name").contains("Windows")
 }
